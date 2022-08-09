68c2cf14b00ec5d43bd51091a1395b342755935d
bernardosan <108026375+bernardosan@users.noreply.github.com>
2022-08-08 22:08:28 -0300

    displaying records on recycler view not implemented yet - edit and delete button

---
+++ app/src/main/java/com/example/roomdemo/ItemAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.roomdemo
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomdemo.databinding.ItemsRowBinding
+
+class ItemAdapter(private val items: ArrayList<EmployeeEntity>,
+                  //private val updateListener:(id:Int)->Unit,
+                 // private val deleteListener:(id:Int)->Unit
+    ): RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+
+    class ViewHolder(binding: ItemsRowBinding) : RecyclerView.ViewHolder(binding.root){
+        val llMain = binding.llMain
+        val tvName = binding.tvName
+        val tvEmail = binding.tvEmail
+        val ivEdit = binding.ivEdit
+        val ivDelete = binding.ivDelete
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemsRowBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val context = holder.itemView.context
+        val item = items[position]
+
+        holder.tvName.text = item.name
+        holder.tvEmail.text = item.email
+
+        if(position % 2 == 0){
+            holder.llMain.setBackgroundColor(ContextCompat.getColor(context, R.color.colorLightGray))
+        } else{
+            holder.llMain.setBackgroundColor(ContextCompat.getColor(context, R.color.white))
+        }
+
+        holder.ivEdit.setOnClickListener {
+           //updateListener.invoke(item.id)
+        }
+
+        holder.ivDelete.setOnClickListener {
+           //deleteListener.invoke(item.id)
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+}
--- app/src/main/java/com/example/roomdemo/MainActivity.kt
+++ app/src/main/java/com/example/roomdemo/MainActivity.kt
@@ -1,45 +1,79 @@
 package com.example.roomdemo
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.roomdemo.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private var binding: ActivityMainBinding? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
         val employeeDao = (application as EmployeeApp).db.employeeDao()
-
         binding?.btnAdd?.setOnClickListener {
-            addRecord(employeeDao = employeeDao)
+            addRecord(employeeDao)
         }
 
+        lifecycleScope.launch {
+            employeeDao.fetchAllEmployees().collect {
+                val list = ArrayList(it)
+                setupListOfDataIntoRecyclerView(list, employeeDao)
+            }
+        }
     }
 
-    fun addRecord(employeeDao: EmployeeDao){
+    private fun setupListOfDataIntoRecyclerView(
+        employeesList: ArrayList<EmployeeEntity>,
+        employeeDao: EmployeeDao
+    ) {
+
+        if (employeesList.isNotEmpty()) {
+            val itemAdapter = ItemAdapter(employeesList)
+
+            // Set the LayoutManager that this RecyclerView will use.
+            binding?.rvItemsList?.layoutManager = LinearLayoutManager(this)
+            binding?.rvItemsList?.adapter = itemAdapter
+            binding?.rvItemsList?.visibility = View.VISIBLE
+            binding?.tvNoRecordsAvailable?.visibility = View.GONE
+        } else {
+
+            binding?.rvItemsList?.visibility = View.GONE
+            binding?.tvNoRecordsAvailable?.visibility = View.VISIBLE
+        }
+    }
+
+
+    //method for saving records in database
+    fun addRecord(employeeDao: EmployeeDao) {
         val name = binding?.etName?.text.toString()
         val email = binding?.etEmailId?.text.toString()
-
-        if (name.isNotEmpty() && email.isNotEmpty()){
-            lifecycleScope.launch{
-                employeeDao.insert(EmployeeEntity(name=name, email = email))
+        if (name.isNotEmpty() && email.isNotEmpty()) {
+            lifecycleScope.launch {
+                employeeDao.insert(EmployeeEntity(name = name, email = email))
                 Toast.makeText(applicationContext, "Record saved", Toast.LENGTH_LONG).show()
                 binding?.etName?.text?.clear()
                 binding?.etEmailId?.text?.clear()
+
             }
-        } else{
-            Toast.makeText(applicationContext, "Name or Email cannot be blank", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(
+                applicationContext,
+                "Name or Email cannot be blank",
+                Toast.LENGTH_LONG
+            ).show()
         }
-
     }
-
-
 }
